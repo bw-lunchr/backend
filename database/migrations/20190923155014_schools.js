c81145b8780f4962484d56968bd373f073b97a25
VulpineReynard <milare8@gmail.com>
2019-09-23 15:56:52 -0400

    added schema for schools

---
+++ database/migrations/20190923155014_schools.js
@@ -0,0 +1,14 @@
+exports.up = function(knex) {
+  return knex.scheme.createTable('schools', table => {
+    table.increments();
+
+    table.string('name', 255).notNullable().unique();
+    table.string('location', 255).notNullable().unique();
+    table.float('requested_funds').notNullable();
+    table.integer('admin_id').unsigned().references('id').inTable('admins')
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('schools');
+};
